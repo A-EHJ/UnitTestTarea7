f4a4d1ad2577e3b242e335a383207238ff21de92
A-EHJ <abrahamelhage2003@gmail.com>
2023-11-08 13:24:04 -0400

    Creando la clase para comprobar la contraseña, creando las funciones.

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Linq;
 
 namespace UnitTestTare7
 {
@@ -9,6 +10,41 @@ namespace UnitTestTare7
         [TestMethod]
         public void TestMethod1()
         {
+        }
+
+
+    }
+
+    internal class RegisterViewModel
+    {
+        internal bool IsPasswordSecure(string contraseña)
+        {
+            if (contraseña.Length < 10)
+            {
+                return false;
+            }
+
+            if (!ContieneMayuscula(contraseña))
+            {
+                return false;
+            }
+
+            if (!ContieneSimbolo(contraseña))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool ContieneMayuscula(string contraseña)
+        {
+            return contraseña.Any(c => Char.IsLetter(c) && Char.IsUpper(c));
+        }
+
+        private bool ContieneSimbolo(string contraseña)
+        {
+            return contraseña.Any(c => !Char.IsSymbol(c));
         }
     }
 }
