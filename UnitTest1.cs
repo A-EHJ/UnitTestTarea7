447ca8336605ee5d3acdb084cd958f424e907343
A-EHJ <abrahamelhage2003@gmail.com>
2023-11-08 13:31:06 -0400

    Creando los metodos de test para comprobar si es segura.

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -7,44 +7,119 @@ namespace UnitTestTare7
     [TestClass]
     public class UnitTest1
     {
+
         [TestMethod]
-        public void TestMethod1()
+        public void EsSegura_return_False_sí_es_menor_que_10()
         {
+            // Arrange
+            var registerViewModel = new RegisterViewModel();
+
+            // Act
+            bool result = registerViewModel.IsPasswordSecure("123456789");
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void EsSegura_return_True_sí_es_mayor_o_igual_que_10()
+        {
+            // Arrange
+            var registerViewModel = new RegisterViewModel();
+
+            // Act
+            bool result = registerViewModel.IsPasswordSecure("123456789@A");
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void EsSegura_return_False_si_no_tiene_o_Mayuscula()
+        {
+            //Arrange
+            var registerViewModel = new RegisterViewModel();
+
+            //Act
+            bool result = registerViewModel.IsPasswordSecure("12345678aeiou");
+
+            //Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void EsSegura_return_True_si_tiene_Mayuscula()
+        {
+            //Arrange
+            var registerViewModel = new RegisterViewModel();
+
+            //Act
+            bool result = registerViewModel.IsPasswordSecure("12345678AeIoU");
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void EsSegura_return_False_si_no_tiene_simbolo()
+        {
+            //Arrange
+            var registerViewModel = new RegisterViewModel();
+
+            //Act
+            bool result = registerViewModel.IsPasswordSecure("12345678aeio");
+
+            //Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void EsSegura_return_True_si_tiene_simbolo()
+        {
+            //Arrange
+            var registerViewModel = new RegisterViewModel();
+
+            //Act
+            bool result = registerViewModel.IsPasswordSecure("12345678AeIoU@");
+
+            //Assert
+            Assert.IsTrue(result);
         }
 
 
-    }
 
-    internal class RegisterViewModel
-    {
-        internal bool IsPasswordSecure(string contraseña)
+
+        internal class RegisterViewModel
         {
-            if (contraseña.Length < 10)
+            internal bool IsPasswordSecure(string contraseña)
             {
-                return false;
+                if (contraseña.Length < 10)
+                {
+                    return false;
+                }
+
+                if (!ContieneMayuscula(contraseña))
+                {
+                    return false;
+                }
+
+                if (!ContieneSimbolo(contraseña))
+                {
+                    return false;
+                }
+
+                return true;
             }
 
-            if (!ContieneMayuscula(contraseña))
+            private bool ContieneMayuscula(string contraseña)
             {
-                return false;
+                return contraseña.Any(c => Char.IsLetter(c) && Char.IsUpper(c));
             }
 
-            if (!ContieneSimbolo(contraseña))
+            private bool ContieneSimbolo(string contraseña)
             {
-                return false;
+                return contraseña.Any(c => !Char.IsSymbol(c));
             }
-
-            return true;
-        }
-
-        private bool ContieneMayuscula(string contraseña)
-        {
-            return contraseña.Any(c => Char.IsLetter(c) && Char.IsUpper(c));
-        }
-
-        private bool ContieneSimbolo(string contraseña)
-        {
-            return contraseña.Any(c => !Char.IsSymbol(c));
         }
     }
 }
